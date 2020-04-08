4c2358eff3e48ef591cb55b493b3164edd387c33
al-beres <61631323+al-beres@users.noreply.github.com>
2020-04-08 01:47:13 -0400

    Add files via upload

--- thomson_problem.py
+++ thomson_problem.py
@@ -36,25 +36,37 @@ def show(points, color = 'blue'):
 
 # TensorFlow 2.1 environmnent
         
+@tf.function
 def tf_vertices_and_edges(points):
     N = len(points)
     vertices = tf.Variable(points, name='points')
     edges = []
-    for i in range(N):
-        for j in range(i + 1, N):
-            edges.append(tf.math.subtract(vertices[i], vertices[j]))
+    #for i in range(N):
+    #    for j in range(i + 1, N):
+    #        edges.append(tf.math.subtract(vertices[i], vertices[j]))
     return vertices, edges
     
+
+@tf.function
 def tf_vertices_and_energy(points, alpha = 2.):
     N = len(points)
-    vertices = tf.Variable(points)
+    vertices = [tf.Variable(p) for p in points]
     edges = []
     for i in range(N):
         for j in range(i + 1, N):
             edges.append(tf.math.subtract(vertices[i], vertices[j]))
-    energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in tf_edges])
+    energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
     return vertices, energy
+
+@tf.function
+def minimize_energy():
     
+
+@tf.function
+def energy():
+    #return tf_energy
+    return tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
+
 
 # Main script
 
@@ -64,28 +76,54 @@ alpha = 3.
 
 points = initial_set(n, d)
 
-tf_vertices, tf_edges = tf_vertices_and_edges(points)
-tf_vertices, tf_energy = tf_vertices_and_energy(points)
+vertices = [tf.Variable(p) for p in points]
+opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+minimization_steps = 1000
+print_at_each = 100
+for step in range(minimization_steps):
+    with tf.GradientTape() as t:
+        edges = []
+        N = len(points)
+        for i in range(N):
+            for j in range(i + 1, N):
+                edges.append(tf.subtract(vertices[i], vertices[j]))
+        energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
+    
+    gradients = t.gradient(energy, vertices)
+    opt.apply_gradients(zip(gradients, vertices))
+    vertices = vertices / la.norm(vertices[0].numpy())
+
+    if step % print_at_each == 0:
+        print('step {:2d}   energy {:1.8f}'.format(step, energy.numpy()))
 
 
-@tf.function
-def energy():
-    return tf_energy
-    return tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in tf_edges])
 
-adam = tf.keras.optimizers.Adam(learning_rate=0.001)
+vertices[0].assign(vertices[0] / 2.)
+edges
 
-op = adam.minimize(energy, tf_vertices)
+
+op = adam.minimize(energy, vertices)
 
 
 op = adam.minimize(tf_energy, tf_vertices)
 
 
+show(vertices, 'blue')
 
 
 
+with tf.GradientTape() as t:
+    energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
 
-
+    #### Option 1
+    
+    # Is the tape that computes the gradients!
+    gradients = t.gradient(energy, vertices)
+    # The optimize applies the update, using the variables
+    # and the optimizer update rule
+    opt.apply_gradients(zip(gradients, vertices))
+    
 
 
 
