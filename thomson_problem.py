61b24f694c3ae15b85b9497020508b856a3f8a81
al-beres <61631323+al-beres@users.noreply.github.com>
2020-04-08 14:56:56 -0400

    Add files via upload

--- thomson_problem.py
+++ thomson_problem.py
@@ -34,8 +34,83 @@ def show(points, color = 'blue'):
         ax.scatter(p[0], p[1], p[2], color=color)
 
 
+
+# Main script
+
+d = 3
+n = 4
+alpha = 3.
+
+points = initial_set(n, d)
+#show(points, 'blue')
+
+
 # TensorFlow 2.1 environmnent
-        
+
+vertices = [tf.Variable(p) for p in points]
+opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+minimization_steps = 1000
+print_at_each = 100
+for step in range(minimization_steps):
+    with tf.GradientTape() as t:
+        edges = []
+        N = len(points)
+        for i in range(N):
+            for j in range(i + 1, N):
+                edges.append(tf.subtract(vertices[i], vertices[j]))
+        energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
+    
+    gradients = t.gradient(energy, vertices)
+    opt.apply_gradients(zip(gradients, vertices))
+    vertices = [tf.Variable(v / la.norm(v.numpy())) for v in vertices]
+
+    if (step + 1) % print_at_each == 0:
+        print('step {:4d}   energy {:1.4f}'.format(step + 1, energy.numpy()))
+
+print('\nfinal energy {:1.4f}'.format(energy.numpy()))
+opt_points = [v.numpy() for v in vertices]
+
+
+fig = pl.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for p in points:
+    ax.scatter(p[0], p[1], p[2], color='blue')
+for p in opt_points:
+    ax.scatter(p[0], p[1], p[2], color='red')
+
+
+
+# surface
+u = np.linspace(0, 2 * np.pi, 200)
+v = np.linspace(0, np.pi, 1000)
+x = 1 * np.outer(np.cos(u), np.sin(v))
+y = 1 * np.outer(np.sin(u), np.sin(v))
+z = 1 * np.outer(np.ones(np.size(u)), np.cos(v))
+elev = 10.0
+rot = 80.0 / 180 * np.pi
+ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='lightgrey', linewidth=0, alpha=0.5)
+
+
+
+
+
+
+
+
+
+# Performance measurements
+
+print("TensorFlow / Adam / CUDA")
+start = time.time()
+opt_points = tf_optimize_adam(points)
+end = time.time()
+print(end - start)
+
+
+
+'''
 @tf.function
 def tf_vertices_and_edges(points):
     N = len(points)
@@ -68,50 +143,6 @@ def energy():
     return tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
 
 
-# Main script
-
-d = 3
-n = 5
-alpha = 3.
-
-points = initial_set(n, d)
-
-vertices = [tf.Variable(p) for p in points]
-opt = tf.keras.optimizers.Adam(learning_rate=0.001)
-
-minimization_steps = 1000
-print_at_each = 100
-for step in range(minimization_steps):
-    with tf.GradientTape() as t:
-        edges = []
-        N = len(points)
-        for i in range(N):
-            for j in range(i + 1, N):
-                edges.append(tf.subtract(vertices[i], vertices[j]))
-        energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
-    
-    gradients = t.gradient(energy, vertices)
-    opt.apply_gradients(zip(gradients, vertices))
-    vertices = vertices / la.norm(vertices[0].numpy())
-
-    if step % print_at_each == 0:
-        print('step {:2d}   energy {:1.8f}'.format(step, energy.numpy()))
-
-
-
-vertices[0].assign(vertices[0] / 2.)
-edges
-
-
-op = adam.minimize(energy, vertices)
-
-
-op = adam.minimize(tf_energy, tf_vertices)
-
-
-show(vertices, 'blue')
-
-
 
 with tf.GradientTape() as t:
     energy = tf.math.add_n([1. / tf.math.reduce_sum(tf.math.abs(e) ** alpha) for e in edges])
@@ -124,33 +155,4 @@ with tf.GradientTape() as t:
     # and the optimizer update rule
     opt.apply_gradients(zip(gradients, vertices))
     
-
-
-
-
-
-
-
-
-
-show(points, 'blue')
-show(opt_points, 'red')
-
-
-
-
-
-
-
-
-
-
-
-
-# Performance measurements
-
-print("TensorFlow / Adam / CUDA")
-start = time.time()
-opt_points = tf_optimize_adam(points)
-end = time.time()
-print(end - start)
+'''
